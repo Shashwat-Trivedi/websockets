f59b850a68d7b547d33a17a2cb07003aeda13a9c
Shashwat-Trivedi <shashwatrivedi2005@gmail.com>
2026-05-21 00:10:02 +0530

    update main entry point to server.js and implement WebSocket server with connection handling

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer,WebSocket } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+// Connection event
+wss.on('connection', (socket , request) => {
+
+    const ip = request.socket.remoteAddress;
+
+    socket.on('message', (rawdata) => {
+        const messaage = rawdata.toString();
+        console.log({messaage});
+
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(`Server Broadcast : ${messaage}`);
+            }
+        });
+    });
+
+    socket.on('error', (error) => {
+        console.error(`Error from client ${ip}:`, error.message);
+    });
+
+    socket.on('close', () => {
+        console.log(`Client ${ip} disconnected`);
+    });
+});
